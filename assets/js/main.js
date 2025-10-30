06a6f0199a72a82b0878f9c9349606cee52ff022
uncgrad1995 <uncgrad1995@gmail.com>
2025-10-30 13:59:59 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-// main.js placeholder
+// Collapsible Player Stats
+document.addEventListener("DOMContentLoaded", function() {
+  const coll = document.querySelectorAll(".collapsible");
+  coll.forEach(button => {
+    button.addEventListener("click", () => {
+      button.classList.toggle("active");
+      const content = button.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  });
+});
